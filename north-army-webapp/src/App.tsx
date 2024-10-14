45115a624bf5489b9648aaf15b4302887cf11904
Eloy Alfredo Schmidt Rodríguez <eloyschmidtrodriguez@gmail.com>
2024-10-14 14:22:35 +0200

    feat: Anadir Mantine

--- north-army-webapp/src/App.tsx
+++ north-army-webapp/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+
+import {MantineProvider} from "@mantine/core";
 
 function App() {
 
   return (
     <>
+      <MantineProvider>
 
+      </MantineProvider>
     </>
   )
 }
